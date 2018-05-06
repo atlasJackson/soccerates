e27a57a98639ca9ba139e2bb58cde9d101f8be91
lsimps204 <semioticghost1@gmail.com>
2018-05-06 13:36:54 +0100

    Registration functionality added

--- socapp/urls.py
+++ socapp/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
 
     path('login', auth_views.LoginView.as_view(template_name="login.html"), name='login'),
     path('logout', auth_views.LogoutView.as_view(), name='logout'),
-    path('register', views.index, name='register') # Change the 2nd param: placeholder for now
+    path('register', views.RegistrationView.as_view(), name='register')
 ]
--- socapp/views.py
+++ socapp/views.py
@@ -1,9 +1,28 @@
+from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
+from django.contrib.messages.views import SuccessMessageMixin
 from django.urls import reverse
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.views.generic.edit import CreateView
 
-# Create your views here.
+from .forms import RegistrationForm
+
 
 def index(request):
     return render(request, "index.html")
+
+class RegistrationView(SuccessMessageMixin, CreateView):
+    template_name = "register.html"
+    form_class = RegistrationForm
+    success_url = "/"
+    success_message = "Registration successful. Welcome, %(username)s"
+
+    # Override to auto-login user when they register
+    def form_valid(self, form):
+        valid = super().form_valid(form)
+        username = self.request.POST['username']
+        pw = self.request.POST['password']
+        user = authenticate(username=username, password=pw)
+        login(self.request, user)
+        return valid
