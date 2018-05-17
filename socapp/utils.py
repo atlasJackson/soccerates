4b14900f24eb58d8ba35e2a6f3fca2d6029cd0c1
lsimps204 <semioticghost1@gmail.com>
2018-05-17 18:27:33 +0100

    Helper method added to get the last game(s) played

--- socapp/utils.py
+++ socapp/utils.py
@@ -8,8 +8,15 @@ from django.utils import timezone
 # Returns the next games to be played, the number of which can be specified, but which defaults to 3
 def get_next_games(number=3):
     cur_date = timezone.now()
+    # Since the Fixture model's default ordering is by match_date, we can just take a slice of the query results
     fixtures = Fixture.objects.filter(status=Fixture.MATCH_STATUS_NOT_PLAYED)[:number]
     for fixture in fixtures:
         assert cur_date < fixture.match_date # If this fails, something has gone wrong.
     return fixtures
 
+def get_last_results(number=3):
+    cur_date = timezone.now()
+    fixtures = Fixture.objects.filter(status=Fixture.MATCH_STATUS_PLAYED).reverse()[:number]
+    for fixture in fixtures:
+        assert cur_date > fixture.match_date
+    return fixtures
