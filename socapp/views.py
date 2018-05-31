e3857454f281753117cb8680b8aeb0bb0dc33693
atlasJackson <le.g.funkadelic@googlemail.com>
2018-05-31 14:44:54 +0100

    Minor edits.

--- socapp/views.py
+++ socapp/views.py
@@ -122,35 +122,38 @@ def answer_form(request):
             # Print problems to the terminal.
             print(answer_formset.errors)
     else:
-        # Not an HTTP POST, so we render our forms.
-        # These forms will be blank, ready for user input, unless the user has already submitted an answer.
-        # But first we check if the user has already submitted answers. If so, we populate the form with the existing answers.
-
-        user_answers = []
-        for fixture in group_fixtures:
-            ans = Answer.objects.select_related('fixture','user') \
-                .filter(fixture=fixture, user=request.user) # Check if user has an answer for this fixture
-            if ans.count() > 0:
-                user_answers.append(ans[0]) # If so add it to the answers list
-            else:
-                user_answers.append(None) # If not, append None to the array so we can ignore it in the listcomp below when setting initial values
         
-        # Set the formset, w/ initial values if an answer exists from the user for the fixture in question
-        formset = AnswerFormSet(initial=[{'team1_goals': a.team1_goals, "team2_goals": a.team2_goals} for a in user_answers if a is not None])
-        management_form = formset.management_form
+        # Check user is logged in, otherwise can't render the formset.
+        if not request.user.is_anonymous:
+            # Not an HTTP POST, so we render our forms.
+            # These forms will be blank, ready for user input, unless the user has already submitted an answer.
+            # But first we check if the user has already submitted answers. If so, we populate the form with the existing answers.
 
-        # The zip Python method takes two (or more) lists/iterables, and binds them together in tuples, based on their index in each list (so the lists should have the same len).
-        # So we're binding each form in the formset with a fixture, and setting the initial value of the form's hidden Fixture field to the associated/'zipped' fixture.
-        # We also create a zipped_groups list, so we can track each fixture/form iteration's group in the template
-        zipped_groups = []
-        for match, form in zip(group_fixtures, formset):
-            form.fields['fixture'].initial = match
-            zipped_groups.append(match.team1.group)
+            user_answers = []
+            for fixture in group_fixtures:
+                ans = Answer.objects.select_related('fixture','user') \
+                    .filter(fixture=fixture, user=request.user) # Check if user has an answer for this fixture
+                if ans.count() > 0:
+                    user_answers.append(ans[0]) # If so add it to the answers list
+                else:
+                    user_answers.append(None) # If not, append None to the array so we can ignore it in the listcomp below when setting initial values
+            
+            # Set the formset, w/ initial values if an answer exists from the user for the fixture in question
+            formset = AnswerFormSet(initial=[{'team1_goals': a.team1_goals, "team2_goals": a.team2_goals} for a in user_answers if a is not None])
+            management_form = formset.management_form
 
-        # With the initial values set within the form, we add the zipped fixtures/forms/groups data structure to the template context. 
-        # This allows us to iterate over each fixture/form in the template, with access to the associated group, and will ensure they're in sync.
-        context_dict['fixtures_and_forms'] = zip(group_fixtures, formset, zipped_groups)
-        context_dict['management_form'] = management_form
+            # The zip Python method takes two (or more) lists/iterables, and binds them together in tuples, based on their index in each list (so the lists should have the same len).
+            # So we're binding each form in the formset with a fixture, and setting the initial value of the form's hidden Fixture field to the associated/'zipped' fixture.
+            # We also create a zipped_groups list, so we can track each fixture/form iteration's group in the template
+            zipped_groups = []
+            for match, form in zip(group_fixtures, formset):
+                form.fields['fixture'].initial = match
+                zipped_groups.append(match.team1.group)
+
+            # With the initial values set within the form, we add the zipped fixtures/forms/groups data structure to the template context. 
+            # This allows us to iterate over each fixture/form in the template, with access to the associated group, and will ensure they're in sync.
+            context_dict['fixtures_and_forms'] = zip(group_fixtures, formset, zipped_groups)
+            context_dict['management_form'] = management_form
 
     return render(request, 'answer_form.html', context_dict)
 
