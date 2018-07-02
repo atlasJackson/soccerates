f2170dd5ea325483cd3c8c63a22310aae83af3cd
atlasJackson <le.g.funkadelic@googlemail.com>
2018-07-02 01:36:55 +0100

    Fixed tooltip display.

--- socapp/templatetags/socapp_filters.py
+++ socapp/templatetags/socapp_filters.py
@@ -103,13 +103,13 @@ def get_free_spaces(leaderboard):
 # Returns the result of the fixture associated with the given fixture.
 @register.filter(name="get_result")
 def get_result(answer):
-    if answer.fixture.has_extra_time: 
-        return "Result: {} {}-{} {} *AET".format(
-            answer.fixture.team1.name, answer.fixture.team1_goals, answer.fixture.team2_goals, answer.fixture.team2.name)
-    elif answer.fixture.has_penalties:
+    if answer.fixture.has_penalties:
         return "Result: {} {} ({})-({}) {} {} *Pen".format(
             answer.fixture.team1.name, answer.fixture.team1_goals, answer.fixture.team1_penalties,
             answer.fixture.team2_penalties, answer.fixture.team2_goals, answer.fixture.team2.name)
+    elif answer.fixture.has_extra_time: 
+        return "Result: {} {}-{} {} *AET".format(
+            answer.fixture.team1.name, answer.fixture.team1_goals, answer.fixture.team2_goals, answer.fixture.team2.name)
     else:
         return "Result: {} {}-{} {}".format(
             answer.fixture.team1.name, answer.fixture.team1_goals, answer.fixture.team2_goals, answer.fixture.team2.name)
